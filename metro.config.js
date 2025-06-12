34059a843f0bff19b94ac532c17075f7fc74f1bc
MarekKabala1 <marekkabala1@gmail.com>
2025-06-12 06:24:04 +0100

    Add initial configuration files for NativeWind and Drizzle ORM, update package dependencies, and adjust TypeScript settings

---
+++ metro.config.js
@@ -0,0 +1,11 @@
+const { getDefaultConfig } = require('expo/metro-config');
+const { withNativeWind } = require('nativewind/metro');
+
+const config = getDefaultConfig(__dirname);
+
+config.resolver.sourceExts.push('sql', 'js', 'jsx', 'ts', 'tsx');
+
+module.exports = withNativeWind(config, {
+	input: './global.css',
+	inlineRem: 16,
+});
